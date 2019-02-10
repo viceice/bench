d4a882d81970e5846ae8ec1bc27a963fe002f9aa
Michael Kriese <michael.kriese@visualon.de>
2019-02-10 12:23:36 +0100

    Added basic ipc project.

---
+++ src/Bench.IPC/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+using System.Reflection;
+
+namespace Bench.IPC
+{
+    class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(GetAssembly()).Run(args);
+
+        private static Assembly GetAssembly() => typeof(Program).GetTypeInfo().Assembly;
+    }
+}
